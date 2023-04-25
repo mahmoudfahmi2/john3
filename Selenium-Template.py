5fd44817671f1d555d14b5d2a279ece6f041c630
mahmoudfahmi2 <126987016+mahmoudfahmi2@users.noreply.github.com>
2023-04-25 21:56:08 +0200

    Update Selenium-Template.py

--- Selenium-Template.py
+++ Selenium-Template.py
@@ -3,6 +3,9 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
 import chromedriver_autoinstaller
 from pyvirtualdisplay import Display
 display = Display(visible=0, size=(800, 800))  
